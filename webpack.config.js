7dd334db6a31e7d06a4348799efde8728ae290a6
miro5460 <robin411989@gmail.com>
2018-10-22 22:32:40 -0400

    react, webpack, babel installed and working

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,29 @@
-const path = require('path');
-const SRC_DIR = path.join(__dirname, '/react-client/src');
-const DIST_DIR = path.join(__dirname, '/react-client/dist');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: `${SRC_DIR}/index.jsx`,
-    output: {
-	path: DIST_DIR,
-	filename: 'bundle.js',
+    entry: './src/index.js',
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
     },
     resolve: {
-	extensions: ['.js', '.jsx', '.json', '.css']
+    extensions: ['*', '.js', '.jsx']
     },
-    module : {
-	rules : [
-{
-    test: /\.css$/,
-    loader: 'style-loader!css-loader'
-},
-{
-    test: /\.png$/,
-    loader: 'url-loader?limit=100000&minetype=image/png'
-},
-{
-    test: /\.jpg/,
-    loader: 'file-loader'
-},
-{
-    test : /\.jsx?/,
-    include : SRC_DIR,
-    loader : 'babel-loader',      
-    query: {
-	presets: ['react', 'es2015']
-    }
-}
-    ]
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.js'
     },
     plugins: [
-	      new webpack.DefinePlugin({
-		      'process.env.NODE_ENV': JSON.stringify('production')
-		  })
-  ]
-	};
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    }
+  };
